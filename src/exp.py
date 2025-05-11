1e80379caa78f7aebe226c335424fdb99477f566
Marshall Ward <marshall.ward@gmail.com>
2025-05-11 01:10:48 -0400

    Very minor cleanup
    Actually just checkpointing while I try again to iterate on remez

--- src/exp.py
+++ src/exp.py
@@ -29,65 +29,47 @@ nodes = bound * cosdg(angles)
 
 ## Check output
 #print(angles)
-#print(nodes)
+print(nodes)
 
 
 #---
+x = np.linspace(-bound, bound, 1000)
 
 # Start the Remez minimax solver.  Set up the first iteration.
 
+# Construct the matrix
 R = np.vander(nodes, N=n_nodes-1, increasing=True)
 # TODO: Better way to do this?
-R = np.append(R,[[(-1)**k] for k in range(n_nodes)],axis=1)
-
+R = np.append(R, [[(-1)**k] for k in range(n_nodes)], axis=1)
 f = np.exp(nodes)
-
-# Now solve for the coefficients
 coeff = np.linalg.solve(R, f)
 
-## Check output
-#print(R)
-#print(f)
-#print(coeff)
-
+# And build the polynomial
 mm_poly = Polynomial(coeff[:-1])
 
-#---
-
-# Check results
-
-# Plot the first estimate
-x = np.linspace(-bound, bound, 100)
 err = np.exp(x) - mm_poly(x)
 
+# Plot the first estimate
 plt.plot(x, err)
 plt.show()
 
-#---
-
-# Try a second iteration of Remez.
-# We need to find the new extrema.
-
-# 1. Find the roots of the error function on a dense grid.
-
-x = np.linspace(-bound, bound, 1000)
-err = np.exp(x) - mm_poly(x)
-
+# Update the positions of the extrema if necessary
+# Find the roots of the error function on a dense grid.
 # TODO: Find a better method
 roots = np.where(np.sign(err[:-1]) != np.sign(err[1:]))[0]
 roots = np.insert(roots, 0, 0)
 roots = np.append(roots, len(err)-1)
-print(roots)
+print("roots?", roots)
 
 # Now find the extrema in each segment
 # TODO: Again, write our own algorithm
-#ext = [l + np.abs(err[l:r]).argmax() for l, r in itertools.pairwise(roots)]
-ext = np.array([x[l + np.abs(err[l:r]).argmax()] for l, r in itertools.pairwise(roots)])
-print(ext)
-
-# And now rebuild the coefficients
-nodes = ext
+nodes = np.array([
+    x[l + np.abs(err[l:r]).argmax()]
+    for l, r in itertools.pairwise(roots)
+])
 n_nodes = len(nodes)
+print("nodes?", nodes)
+
 R = np.vander(nodes, N=n_nodes-1, increasing=True)
 R = np.append(R,[[(-1)**k] for k in range(n_nodes)],axis=1)
 f = np.exp(nodes)
@@ -95,13 +77,10 @@ f = np.exp(nodes)
 # Now solve for the coefficients
 coeff = np.linalg.solve(R, f)
 
-## Check output
-#print(R)
-#print(f)
-#print(coeff)
-
 mm_poly = Polynomial(coeff[:-1])
 
+err = np.exp(x) - mm_poly(x)
+
 # And check it again
-plt.plot(np.exp(x) - mm_poly(x))
+plt.plot(x, err)
 plt.show()
