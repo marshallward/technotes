fd5ce2ebc1d21c4318f5c0e36d7aff89bec848fb
Marshall Ward <marshall.ward@gmail.com>
2025-06-15 14:57:50 -0400

    Working gauss echelon solver
    Not actually a solver ofc, ignores the RHS!

--- lu/gauss.py
+++ lu/gauss.py
@@ -5,15 +5,14 @@ import scipy
 #np.random.seed(0)
 #A = np.random.rand(4,4)
 
-# Textbook example
-A = np.array([[2., 1., -1.], [-3., -1., 2.], [-2., 1., 2.]])
+## Textbook example
+#A = np.array([[2., 1., -1.], [-3., -1., 2.], [-2., 1., 2.]])
 
 ## A FP-sensitive example?
 #A = scipy.linalg.hilbert(8)
 
-## A pivoting example
-#A = np.array([[2., 1., -1.], [4., 2., -2.], [-2., 1., 2.]])
-A = np.array([[2., 1., -1.], [-2., 1., 2.], [4., 2., -2.]])
+# A pivoting example
+A = np.array([[2., 1., -1.], [4., 2., -2.], [-2., 1., 2.]])
 
 print("A = ")
 print(A)
@@ -23,9 +22,6 @@ n = A.shape[0]
 
 E = np.empty((n,n))
 
-# Copy A to E
-#E[0,:] = A[0,:]
-
 # To compute the echelon form of A, we march across the columns of A.
 # For diagonal point A[i,i]:
 #   For row j >= 1:
@@ -33,17 +29,38 @@ E = np.empty((n,n))
 #        
 #   This sets column A[i:,i+1:] to zero, and modifies the rest.
 
-# Implementation notes:
-# - The previous iteration sets E[:i,j] = 0, so we 
-# - It may be numerically preferable to explicitly set E[i:,j] to zero?
-# -     Does this help or hurt accuracy?
+
 
 # Compute the echelon (upper triangular) form of A
+
+# This is like the "first term" in each element of E
 E[:,:] = A[:,:]
-#E[0,:] = A[0,:]
-for i in range(1,n):
-    c = E[i:,i-1:i] / E[i-1,i-1]
-    E[i:,:] = E[i:,:] - c * E[i-1:i,:]
+
+for i in range(n-1):
+    #print("i=", i)
+    ## Collect the i column coefficient, weighted by the i diagonal
+    #c = E[i+1:,i:i+1] / E[i,i]
+    #print("c=")
+    #print(c)
+
+    ## Compute the diff row
+    #r = E[i:i+1,i:]
+    #print("r=")
+    #print(r)
+    
+    ## Apply to all i+1 rows, which should force E[i+1:,i] to zero
+    #E[i+1:,i:] = E[i+1:,i:] - c * r
+    #print("E=")
+    #print(E)
+
+    ## Probably faster to do as a single line
+    #E[i+1:,i:] = E[i+1:,i:] - (E[i+1:,i:i+1] / E[i,i]) * E[i:i+1,i:]
+
+    # This expression also loops over the E[:i,:].
+    #   These should all be zero, so it should be numerically equivalent.
+    #   It may be faster, despite the extra work, due to pipelining and
+    #   vectorization.
+    E[i+1:,:] = E[i+1:,:] - (E[i+1:,i:i+1] / E[i,i]) * E[i:i+1,:]
 
 # E is in echelon form
 print("E = ")
@@ -65,6 +82,5 @@ E_check = gaussian_elim(A)
 if np.allclose(E, E_check):
     print("Matches gaussin_elim()")
 else:
-    print(E_check)
     print("Does not match gaussian_elim()")
-
+    print(E_check)
