1a78347edc5b6a25c7887dac9e328ae4f6752420
Marshall Ward <marshall.ward@gmail.com>
2025-06-17 19:29:49 -0400

    LU cleanup

--- lu/lu.py
+++ lu/lu.py
@@ -33,31 +33,11 @@ E = np.empty((n,n))
 # Compute the echelon (upper triangular) form of A
 
 # This is like the "first term" in each element of E
-E = A.copy()
+U = A.copy()
 P = np.eye(n)
 L = np.eye(n)
 
 for i in range(n-1):
-    #if debug:
-    #    print("i=", i)
-    #    # Collect the i column coefficient, weighted by the i diagonal
-    #    c = E[i+1:,i:i+1] / E[i,i]
-    #    print("c=")
-    #    print(c)
-
-    #    # Compute the diff row
-    #    r = E[i:i+1,i:]
-    #    print("r=")
-    #    print(r)
-    #
-    #    # Apply to all i+1 rows, which should force E[i+1:,i] to zero
-    #    E[i+1:,i:] = E[i+1:,i:] - c * r
-    #    print("E=")
-    #    print(E)
-
-    ## Probably faster to do as a single line
-    #E[i+1:,i:] = E[i+1:,i:] - (E[i+1:,i:i+1] / E[i,i]) * E[i:i+1,i:]
-
     # Partial pivot
     # - Search final i rows (including i) for the max coefficient.
     #   - Not that we include the offset to global index
@@ -65,40 +45,29 @@ for i in range(n-1):
     #pivot = np.argmax(E[i:,i]) + i
     #if pivot != i:
 
-    col = np.abs(E[i:,i])
+    col = np.abs(U[i:,i])
     pivot_offset = np.argmax(col)
     pivot = i + pivot_offset
 
     if pivot != i and not np.isclose(col[pivot_offset], col[0], atol=1e-12):
-        E[[i, pivot]] = E[[pivot, i]]
+        U[[i, pivot]] = U[[pivot, i]]
         P[[i, pivot]] = P[[pivot, i]]
 
         # swap the previous L rows up to column i
         # huh??
         if i > 0:
             L[[i, pivot], :i] = L[[pivot, i], :i]
-        #L[[i, pivot], :i] = L[[pivot, i], :i]
 
-    ## This expression also loops over the E[:i,:].
-    ## E[:i,:] = 0, so it should be numerically equivalent.  It may be faster,
-    ## despite the extra work, due to pipelining and vectorization.
-    ## But I haven't yet tested.
-    #E[i+1:,:] = E[i+1:,:] - (E[i+1:,i:i+1] / E[i,i]) * E[i:i+1,:]
+    # The cancellation terms become the L matrix.
+    L[i+1:,i:i+1] = U[i+1:,i:i+1] / U[i,i]
 
-    ## This equivalent but relies on implict reshape of E[i,:] from (n,) to (1,n)
-    ## (I don't think it works if I do pivot swaps)
-    #E[i+1:,:] = E[i+1:,:] - (E[i+1:,i:i+1] / E[i,i]) * E[i,:]
-
-    # This method moves the correction terms into a separate matrix (L)
-    L[i+1:,i:i+1] = E[i+1:,i:i+1] / E[i,i]
-
-    # Then independently apply it to E
-    E[i+1:,:] = E[i+1:,:] - L[i+1:,i:i+1] * E[i:i+1,:]
+    # Apply to E to make it upper triangular.
+    U[i+1:,:] = U[i+1:,:] - L[i+1:,i:i+1] * U[i:i+1,:]
 
 
 # E is in echelon form
 print("U = ")
-print(E)
+print(U)
 print("L = ")
 print(L)
 print("P = ")
@@ -107,11 +76,11 @@ print(P)
 
 # Check the answer
 
-if np.allclose(P @ L @ E, A):
+if np.allclose(P @ L @ U, A):
     print("P L U = A")
 else:
     print("ERROR! P L U /= A!")
     print("A=")
     print(A)
     print("P @ L @ U =")
-    print(P @ L @ E)
+    print(P @ L @ U)
