799019dcfcd7021c3e8e36793aff866428a1a697
Marshall Ward <marshall.ward@noaa.gov>
2026-06-30 14:53:26 -0400

    Add example Fortran exp() implementation

--- remez/exp_repro.f90
+++ remez/exp_repro.f90
@@ -1,39 +1,38 @@
+module exp_repro
+
+use iso_fortran_env, only : real64, real128
+
 implicit none
 
-real(kind=16) :: q
-
-print *, exp(-3.1)
-print *, exp_cr(-3.1)
-
-q = -3.1
-print *, exp(q)
-
-q = 2.
-print *, 1./log(q)
-print *, 2./log(q)
+!*!print *, exp(1000._16)
+!*!!print *, exp(1000.)
+!*!print *, exp_cr(1000.)
+!*!
+!*!print *, exp(0.)
+!*!print *, exp_cr(0.)
 
 contains
 
 elemental function exp_cr(x) result(a)
-  real, intent(in) :: x
+  real(kind=real64), intent(in) :: x
     !< Input value [nondim]
-  real :: a
+  real(kind=real64) :: a
     !< Exponential of x [nondim]
 
-  real :: r
+  real(kind=real64) :: r
     ! Rescaled value of x: r = x - K * ln 2
   integer :: K
     ! Scaling factor, where exp(x) = 2^K exp(r)
-  real :: e
+  real(kind=real64) :: e
     ! Scaled result: exp(r)
 
-  ! TODO: Verify reproducible bit/hex values
-  real, parameter :: INV_LN2 = 1.4426950408889634
-  real, parameter :: TWO_INV_LN2 = 2.8853900817779268
+  ! TODO: Specify as hex to avoid ambiguous rounding
+  real(kind=real64), parameter :: INV_LN2 = 1.4426950408889634_real64
   !real, parameter :: INV_LN2 = 1.44269504088896340735992468100189204
+  real(kind=real64), parameter :: TWO_INV_LN2 = 2.8853900817779268_real64
   !real, parameter :: TWO_INV_LN2 = 2.88539008177792681471984936200378409
-  real, parameter :: LN2_HI = 6.93147180369123816490e-01
-  real, parameter :: LN2_LO = 1.90821492927058770002e-10
+  real(kind=real64), parameter :: LN2_HI = 6.93147180369123816490e-01_real64
+  real(kind=real64), parameter :: LN2_LO = 1.90821492927058770002e-10_real64
 
   ! For Chebyshev evaluation
 
@@ -42,36 +41,50 @@ elemental function exp_cr(x) result(a)
   r = (x - K * LN2_HI) - K * LN2_LO
 
   ! Compute exp(r)
-  ! TODO: Explain rescale to [-1,1]
-  e = exp_remez(r * TWO_INV_LN2)
+  ! In order to force exp(0) = 1, we actually approximate (exp(r) - 1) / r.
+  e = 1. + r * expm1_x_remez(r * TWO_INV_LN2)
 
   ! Descale 
   a = scale(e, K)
 
 end function exp_cr
 
-pure function exp_remez(x) result(e)
-  real, intent(in) :: x
+pure function expm1_x_remez(x) result(e)
+  real(kind=real64), intent(in) :: x
     !< Input value
-  real :: e
+  real(kind=real64) :: e
     !< Approximation of exp(x)
 
-  real :: b0, b1, b2
+  real(kind=real64) :: b0, b1, b2
   integer :: n
 
-  real, parameter :: t(0:11) = [ &
-    1.0302544918096181e+00, &
-    3.5180320783770408e-01, &
-    3.0330010354096444e-02, &
-    1.7475636139768675e-03, &
-    7.5594039827123609e-05, &
-    2.6172719073043696e-06, &
-    7.5535800680132951e-08, &
-    1.8689063234385808e-09, &
-    4.0465189925824025e-11, &
-    7.7884384800120311e-13, &
-    1.3533787921576843e-14, &
-    2.5408732182498882e-16 &
+  !real, parameter :: t(0:10) = [ &
+  !  1.0100546303736508e+00, &
+  !  1.7459202119340825e-01, &
+  !  1.0069712517838964e-02, &
+  !  4.3580091845530559e-04, &
+  !  1.5092920250821286e-05, &
+  !  4.3566706207388448e-07, &
+  !  1.0780554497188584e-08, &
+  !  2.3343806108994727e-10, &
+  !  4.4933851094935047e-12, &
+  !  7.7884991972921424e-14, &
+  !  1.2375905543948744e-15 &
+  !]
+
+  real(kind=real64), parameter :: t(0:11) = [ &
+    1.0100546303736506e+00_real64, &
+    1.7459202119340830e-01_real64, &
+    1.0069712517839037e-02_real64, &
+    4.3580091845535839e-04_real64, &
+    1.5092920250787356e-05_real64, &
+    4.3566706211233559e-07_real64, &
+    1.0780554531543968e-08_real64, &
+    2.3343813394501564e-10_real64, &
+    4.4933658510432855e-12_real64, &
+    7.7841956207353226e-14_real64, &
+    1.1969180485188496e-15_real64, &
+    3.7974225925737191e-17_real64 &
   ]
 
   b1 = 0.0
@@ -82,7 +95,8 @@ pure function exp_remez(x) result(e)
     b2 = b1
     b1 = b0
   enddo
-  e = t(0) + x * b1 - b2
-end function exp_remez
 
-end
+  e = t(0) + x * b1 - b2
+end function expm1_x_remez
+
+end module exp_repro
--- remez/test_remez.py
+++ remez/test_remez.py
@@ -1,11 +1,19 @@
 from remez import remez
 import numpy as np
 import matplotlib.pyplot as plt
+import gmpy2
 
 n = 9
 a, b = -0.5 * np.log(2), 0.5 * np.log(2)
 
-func = np.sin
+def g(x):
+    return np.where(x == 0, 1.0, np.expm1(x) / x)
+
+def g_hi(x):
+    return gmpy2.expm1(x) / x if x != 0 else gmpy2.mpfr(1)
+
+#func = np.sin
+#func = np.exp
 #p, roots, nodes, x  = remez(func, n, domain=(a, b), basis='chebyshev')
 ##p, roots, nodes, x  = remez(func, n, domain=(a, b), basis='monomial')
 #
@@ -15,8 +23,11 @@ func = np.sin
 #plt.hlines(0, x.min(), x.max())
 #plt.show()
 
-p = remez(func, n, domain=(a,b), basis='chebyshev')
+#p = remez(func, n, domain=(a,b), basis='chebyshev')
+#p = remez(g, 10, domain=(a,b), basis='chebyshev')
+p = remez(g, 11, domain=(a,b), basis='chebyshev')
 
 x = np.linspace(a, b, 1024)
-plt.plot(x, p(x) - func(x))
+#plt.plot(x, p(x) - func(x))
+plt.plot(x, x * p(x) - x * g(x))
 plt.show()
