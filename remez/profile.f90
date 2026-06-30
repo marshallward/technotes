d0857aa487f80f08ffe5fae903e3d54d61504ab3
Marshall Ward <marshall.ward@noaa.gov>
2026-06-30 14:54:44 -0400

    exp() profiling test

---
+++ remez/profile.f90
@@ -0,0 +1,61 @@
+use iso_fortran_env, only : real64, real128
+use exp_repro
+
+implicit none
+
+integer, parameter :: niter = 20
+
+real(kind=real64), parameter :: xmax = 0.5 * log(2.)
+real(kind=real128), allocatable :: xq(:), rq(:)
+
+real(kind=real64), allocatable :: x(:), r(:)
+real(kind=real128), parameter :: xqmax = 0.5 * log(2.)
+
+integer :: count_rate, count_max, c1, c2
+real(kind=real64) :: clock_rate
+
+integer :: i, n
+
+n = 1000000
+allocate(x(n), r(n))
+allocate(xq(n), rq(n))
+
+x = [(-xmax + (i - 1) * (2. * xmax) / (n-1), i=1,n)]
+xq = [(-xqmax + (i - 1) * (2. * xqmax) / (n-1), i=1,n)]
+
+!print *, x
+
+! Reference results
+rq = exp(xq)
+
+! Set up clock
+call system_clock(count_rate=count_rate, count_max=count_max)
+clock_rate = real(count_rate)
+
+! Prime the pump
+r = exp(x)
+
+! Evaluate exp() niter times
+call system_clock(count=c1)
+do i = 1, niter
+  r = exp(x)
+end do
+call system_clock(count=c2)
+
+! Report results
+print '(a16,1x,g0)', "exp():", (c2 - c1) / clock_rate / niter
+print '(a16,1x,g0)', "  err:", maxval(abs(r - rq))
+
+! repro version?
+r = exp_cr(x)
+call system_clock(count=c1)
+do i = 1, niter
+  r = exp_cr(x)
+end do
+call system_clock(count=c2)
+
+! Report results
+print '(a16,1x,g0)', "exp_repro():", (c2 - c1) / clock_rate / niter
+print '(a16,1x,g0)', "        err:", maxval(abs(r - rq))
+
+end
